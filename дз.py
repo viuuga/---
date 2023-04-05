956a0ea3c44fd3002aaeff3f9896d422c17a54b3
Александр <124909928+viuuga@users.noreply.github.com>
2023-04-05 15:25:18 +0300

    Delete дз.py

--- дз.py
+++
@@ -1,9 +0,0 @@
-def print_all (*args,  sep = ' ', end = '\n'):
-    new_str = ''
-    for arg in args:
-        new_str += str(arg) + sep
-    new_str = new_str.rstrip(sep) + end
-    return  new_str
-
-print(print_all(1, 2, 3, 4, sep = '****', end = '@'))
-print(print_all(1, 2, 3, 4, 5, end = '!!!'))
